f084d1fcbd5e35d5b0538cb163ec50ae2fc61cca
TukiyoIgarashi <tukiyo320@gmail.com>
2017-02-28 19:18:48 +0900

    make it kotlin

---
+++ app/src/main/kotlin/jp/co/tukiyo/yoruha/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.tukiyo.yoruha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
