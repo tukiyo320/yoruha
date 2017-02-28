8d52ed641a3da32b028f676ff692dfe78246e52e
TukiyoIgarashi <tukiyo320@gmail.com>
2017-02-28 20:50:54 +0900

    move check uuid to ScreenActivity

--- app/src/main/kotlin/jp/co/tukiyo/yoruha/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/jp/co/tukiyo/yoruha/ui/activity/MainActivity.kt
@@ -3,24 +3,13 @@ package jp.co.tukiyo.yoruha.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import jp.co.tukiyo.yoruha.extensions.getSharedPreference
-import jp.co.tukiyo.yoruha.extensions.getUuid
-import jp.co.tukiyo.yoruha.extensions.putUuid
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getSharedPreference().run {
-            if (getUuid().isEmpty()) {
-                edit()
-                    .putUuid(UUID.randomUUID().toString())
-                    .apply()
-            }
-        }
-
         startActivity(Intent(this, ScreenActivity::class.java))
+        finish()
     }
 
 }
--- app/src/main/kotlin/jp/co/tukiyo/yoruha/ui/activity/ScreenActivity.kt
+++ app/src/main/kotlin/jp/co/tukiyo/yoruha/ui/activity/ScreenActivity.kt
@@ -1,8 +1,13 @@
 package jp.co.tukiyo.yoruha.ui.activity
 
+import android.os.Bundle
 import jp.co.tukiyo.yoruha.R
 import jp.co.tukiyo.yoruha.databinding.ActivityScreenBinding
+import jp.co.tukiyo.yoruha.extensions.getSharedPreference
+import jp.co.tukiyo.yoruha.extensions.getUuid
+import jp.co.tukiyo.yoruha.extensions.putUuid
 import jp.co.tukiyo.yoruha.ui.screen.Screen
+import java.util.*
 
 class ScreenActivity : BaseActivity<ActivityScreenBinding>() {
     override val layoutResourceId: Int = R.layout.activity_screen
@@ -25,5 +30,19 @@ class ScreenActivity : BaseActivity<ActivityScreenBinding>() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.screen, screen.fragment, screen.identify)
                 .commitNow()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        checkUuid()
+    }
+
+    fun checkUuid() {
+        getSharedPreference().run {
+            if (getUuid().isEmpty()) {
+                edit().putUuid(UUID.randomUUID().toString()).apply()
+            }
+        }
     }
 }
