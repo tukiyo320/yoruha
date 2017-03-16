9d72acd4748a180708855fcdd06456ff2e4c379e
TukiyoIgarashi <tukiyo320@gmail.com>
2017-03-16 13:18:33 +0900

    remove unused code

--- app/src/main/kotlin/jp/co/tukiyo/yoruha/api/googlebooks/GoogleBooksAPIClient.kt
+++ app/src/main/kotlin/jp/co/tukiyo/yoruha/api/googlebooks/GoogleBooksAPIClient.kt
@@ -1,19 +1,10 @@
 package jp.co.tukiyo.yoruha.api.googlebooks
 
-import android.accounts.Account
-import android.content.Context
-import android.widget.Toast
-import com.google.android.gms.auth.GoogleAuthUtil
-import com.google.android.gms.auth.UserRecoverableAuthException
 import io.reactivex.Completable
-import io.reactivex.Observable
 import io.reactivex.Single
 import jp.co.tukiyo.yoruha.api.googlebooks.model.BookShelfVolumesResponse
 import jp.co.tukiyo.yoruha.api.googlebooks.model.BooksResponse
 import jp.co.tukiyo.yoruha.api.googlebooks.model.VolumeItem
-import jp.co.tukiyo.yoruha.extensions.getSharedPreference
-import jp.co.tukiyo.yoruha.extensions.getUserEmail
-import okhttp3.ResponseBody
 import retrofit2.http.*
 
 interface GoogleBooksAPIClient {
@@ -21,23 +12,6 @@ interface GoogleBooksAPIClient {
     enum class OrderBy(var command: String) {
         RELEVANCE("relevance"),
         NEWEST("newest");
-    }
-
-    fun getToken(context: Context): Single<String> {
-        val email = context.getSharedPreference().getUserEmail()
-        val scope = "https://www.googleapis.com/auth/books"
-
-        return Single.create { subscriber ->
-            try {
-                val token = GoogleAuthUtil.getToken(context, Account(email, "com.google"), "oauth2:$scope")
-                subscriber.onSuccess(token)
-            } catch (e: UserRecoverableAuthException) {
-                Toast.makeText(context, "authorize failed", Toast.LENGTH_SHORT).show()
-                subscriber.onError(e)
-            } catch (e: Exception) {
-                subscriber.onError(e)
-            }
-        }
     }
 
     @GET("/books/v1/volumes")
